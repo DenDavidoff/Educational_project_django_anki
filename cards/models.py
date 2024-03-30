de21dd2ef0aa685fe2ee949fd3d45a83d4c4f5a9
DenDavidoff <dj.ato@bk.ru>
2024-03-30 17:20:16 +0300

    Создал отдельную ветку migrate в которой подключил новую БД Описал модели в соответствии с новой БД Сделал миграции

--- cards/models.py
+++ cards/models.py
@@ -2,23 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Tag(models.Model):
-    name = models.CharField(max_length=75, unique=True)
-
-    class Meta:
-        verbose_name = 'Тег'  # имя модели в единственном числе
-        verbose_name_plural = 'Теги'  # имя модели во множественном числе
-
-    def __str__(self):
-        return f'Тег {self.name}'
-
 class Card(models.Model):
-    question = models.CharField(max_length=255)
-    answer = models.TextField(max_length=5000)
-    date = models.DateTimeField(auto_now_add=True)
-    views = models.IntegerField(default=0)
-    adds = models.IntegerField(default=0)
-    tags = models.JSONField(default=list)
+    id = models.AutoField(primary_key=True, db_column='CardID')
+    question = models.CharField(max_length=255, db_column='Question')
+    answer = models.TextField(max_length=5000, db_column='Answer')
+    date = models.DateTimeField(auto_now_add=True, db_column='Date')
+    views = models.IntegerField(default=0, db_column='Views')
+    adds = models.IntegerField(default=0, db_column='Favorites')
+    tags = models.ManyToManyField('Tag', through='CardTag', related_name='cards')
 
     class Meta:
         db_table = 'Cards' # имя таблицы в базе данных
@@ -27,25 +18,42 @@ class Card(models.Model):
 
     def __str__(self):
         return f'Карточка {self.question} - {self.answer[:50]}'
+    
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True, db_column='TagID')
+    name = models.CharField(max_length=75, db_column='Name')
 
-"""
-### CRUD Операции с этой моделью
-1. Создание записи
-card = Card(question='Пайтон или Питон?!', answer='Пайтон')
-card.save()
+    class Meta:
+        db_table = 'Tags'
+        verbose_name = 'Тег'  # имя модели в единственном числе
+        verbose_name_plural = 'Теги'  # имя модели во множественном числе
 
-2. Чтение записи
-card = Card.objects.get(pk=1)
-Мы можем добыть любые данные из записи, просто обратившись к атрибутам модели:
-card.question
-card.answer
-card.upload_date
+    def __str__(self):
+        return f'Тег {self.name}'
 
-3. Обновление записи
-card = Card.objects.get(pk=1)
-card.question = 'Питон или Пайтон?!!'
+class CardTag(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, db_column='CardID')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, db_column='TagID')
+    
+    class Meta:
+        db_table = 'CardTags'
+        verbose_name = 'Тег карточки'  # имя модели в единственном числе
+        verbose_name_plural = 'Теги карточек'  # имя модели во множественном числе
+        
+        unique_together = ('card', 'tag')
 
-4. Удаление записи
-card = Card.objects.get(pk=1)
-card.delete()
-"""
+    def __str__(self):
+        return f'Тег {self.tag.name} к карточке {self.card.question}'
+    
+    
+class Category(models.Model):
+    id = models.AutoField(primary_key=True, db_column='CategoryID')
+    name = models.CharField(max_length=120, db_column='Name')
+
+    class Meta:
+        db_table = 'Categories'
+        verbose_name = 'Категория'  # имя модели в единственном числе
+        verbose_name_plural = 'Категории'  # имя модели во множественном числе
+
+    def __str__(self):
+        return f'Категория {self.name}'
