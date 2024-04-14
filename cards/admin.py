589ca3cd245e5c2fef2098ba38d96f52568519ac
DenDavidoff <dj.ato@bk.ru>
2024-04-14 15:07:16 +0300

    Переработал админку и починил миграции

--- cards/admin.py
+++ cards/admin.py
@@ -1,15 +1,34 @@
 from django.contrib import admin
-from .models import Card, Category, Tag
+from .models import Card, Category, Tag, CardTag
 
 # Register your models here.
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'question', 'category', 'views', 'date', 'check_status')
+    list_filter = ('check_status',)
+    list_per_page = 25
+    list_editable = ('check_status',)
+    actions = ['make_checked', 'make_unchecked']
+
+    @admin.action(description='Отметить выбранные карточки как проверенные')
+    def make_checked(self, request, queryset):
+        updated_count = queryset.update(check_status=True)
+        self.message_user(request, f"{updated_count} записей было помечено как проверенное")
+        
+    @admin.action(description='Отметить выбранные карточки как непроверенные')
+    def make_unchecked(self, request, queryset):
+        updated_count = queryset.update(check_status=False)
+        self.message_user(request, f"{updated_count} записей было помечено как непроверенное", 'warning')
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CardTag)
 class CategoryAdmin(admin.ModelAdmin):
     pass
--- cards/models.py
+++ cards/models.py
@@ -3,13 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Card(models.Model):
-    id = models.AutoField(primary_key=True, db_column='CardID')
-    question = models.CharField(max_length=255, db_column='Question')
-    answer = models.TextField(max_length=5000, db_column='Answer')
-    date = models.DateTimeField(auto_now_add=True, db_column='Date')
-    views = models.IntegerField(default=0, db_column='Views')
-    adds = models.IntegerField(default=0, db_column='Favorites')
-    tags = models.ManyToManyField('Tag', through='CardTag', related_name='cards')
+    class Status(models.IntegerChoices):
+        UNCHECKED = 0, 'Не проверено'
+        CHECKED = 1, 'Проверено'
+        
+    id = models.AutoField(primary_key=True, db_column='CardID', verbose_name='ID')
+    question = models.CharField(max_length=255, db_column='Question', verbose_name='Вопрос')
+    answer = models.TextField(max_length=5000, db_column='Answer', verbose_name='Ответ')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, db_column='CategoryID', verbose_name='Категория')
+    date = models.DateTimeField(auto_now_add=True, db_column='Date', verbose_name='Дата загрузки')
+    views = models.IntegerField(default=0, db_column='Views', verbose_name='Просмотры')
+    adds = models.IntegerField(default=0, db_column='Favorites', verbose_name='В избранном')
+    tags = models.ManyToManyField('Tag', through='CardTag', related_name='cards', verbose_name='Теги')
+    check_status = models.BooleanField(default=False, choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), verbose_name='Проверено')
+
+
 
     class Meta:
         db_table = 'Cards' # имя таблицы в базе данных
@@ -18,6 +26,9 @@ class Card(models.Model):
 
     def __str__(self):
         return f'Карточка {self.question} - {self.answer[:50]}'
+    
+    def get_absolute_url(self):
+        return f'/cards/catalog/{self.id}/detail/'
     
 class Tag(models.Model):
     id = models.AutoField(primary_key=True, db_column='TagID')
@@ -32,6 +43,7 @@ class Tag(models.Model):
         return f'Тег {self.name}'
 
 class CardTag(models.Model):
+    id = models.AutoField(primary_key=True, db_column='id')
     card = models.ForeignKey(Card, on_delete=models.CASCADE, db_column='CardID')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, db_column='TagID')
     
