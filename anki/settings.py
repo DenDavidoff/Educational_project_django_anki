e2f0ee16bb887826c82929708fa6a9d9516a68f3
DenDavidoff <dj.ato@bk.ru>
2024-05-27 17:26:13 +0300

    Внесение изменений в ALLOWED_HOSTS

--- anki/settings.py
+++ anki/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dendavidoff-educational-project-django-anki-e55e.twc1.net', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://dendavidoff-educational-project-django-anki-e55e.twc1.net', 'dendavidoff-educational-project-django-anki-e55e.twc1.net', 'www.dendavidoff-educational-project-django-anki-e55e.twc1.net', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://dendavidoff-educational-project-django-anki-e55e.twc1.net']
 
